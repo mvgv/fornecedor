efd66353828bb7d75258e43d7d13829304dc3396
mvgv <mvgv1989@gmail.com>
2019-11-30 00:51:58 -0300

    Commit inicial do app+

---
+++ src/test/java/com/microservicos/fornecedor/fornecedor/FornecedorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicos.fornecedor.fornecedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FornecedorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
